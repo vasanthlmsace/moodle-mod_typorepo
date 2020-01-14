c05afac3ab857b4a137b9df1b747f1b134c30050
moodledev <moodledev@gmail.com>
2020-01-14 10:49:37 -0600

    Add required plugin files

---
+++ db/access.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Capability definitions for the typo3 module.
+ *
+ * @package   mod_typo3
+ * @copyright 2019 bdecent gmbh <https://bdecent.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$capabilities = [
+    'mod/typo3:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'guest' => CAP_ALLOW,
+            'user' => CAP_ALLOW,
+        ]
+    ],
+    'mod/typo3:addinstance' => [
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ],
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ],
+];
---
+++ lang/en/typo3.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'survey', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   mod_typo3
+ * @copyright 2019 bdecent gmbh <https://bdecent.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['displaysettings'] = 'Typo3 repository display settings';
+$string['frameset'] = 'Display typo3 repository in a frameset';
+$string['framesetdesc'] = 'Display typo3 repository in a frameset (same as resource).';
+$string['height'] = 'Height';
+$string['heightdesc'] = 'iframe or new window height in pixels.';
+$string['iframe'] = 'Display typo3 repository in an iframe';
+$string['iframedesc'] = 'Display typo3 repository in an iframe. Will use height/width from input below.';
+$string['integrationsettings'] = 'Typo3 repository integration settings';
+$string['linksaved'] = 'Link saved';
+$string['modulename'] = 'Typo3 repository';
+$string['modulenameadd'] = 'Add a typo3 page';
+$string['modulenameplural'] = 'Typo3 pages';
+$string['modulenameshort'] = 'Typo3 repository';
+$string['newwindow'] = 'Display typo3 repository in a new window';
+$string['newwindowdesc'] = 'Display typo3 repository in a new window. Will use height/width from input below.';
+$string['opennewwindow'] = 'If window does not open, please click here';
+$string['pluginadministration'] = 'Typo3 repository administration';
+$string['pluginname'] = 'Typo3 repository';
+$string['scrolling'] = 'Scrolling';
+$string['scrollingdesc'] = 'iframe scrolling: yes/no/auto.';
+$string['typopageurl'] = 'Typo3 page base url';
+$string['typopageurldesc'] = 'URL to the Typo3 index page from which every page is displayed. It will be appended by all secured id etc parameters automatically.';
+$string['typo3:addinstance'] = 'Add a new Typo3 instance';
+$string['typo3:view'] = 'View Typo3 instance';
+$string['typosecret'] = 'Integration secret';
+$string['typosecretdesc'] = 'Secret phrase for integration encryption token generation.';
+$string['url'] = 'Typo3 repository url';
+$string['urldesc'] = 'URL to the Typo3 moodle ready sitemap module. It will be appended by all secured id etc parameters automatically.';
+$string['width'] = 'Width';
+$string['widthdesc'] = 'iframe or new window width in pixels.';
