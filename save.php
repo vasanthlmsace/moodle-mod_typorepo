d40bbdc83927f6b5f5ccd23e0f30939ae3971518
moodledev <moodledev@gmail.com>
2020-01-21 10:19:43 -0600

    #5 Change javascript (again)

--- save.php
+++ save.php
@@ -34,7 +34,7 @@ echo <<<EOT
 <html>
     <head>
         <script language="javascript">
-            var mform1 = document.getElementsByClassName('mform')[0];
+            var mform1 = parent.document.getElementsByClassName('mform')[0];
             if( typeof mform1.url != 'undefined' )           { mform1.url.value = '$url'; }
             if( typeof mform1.name != 'undefined' )          { mform1.name.value = '$name'; }
             if( typeof mform1.config_url != 'undefined' )    { mform1.config_url.value = '$url'; }
