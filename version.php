4888e5ffad12cde24520bb4ed68fd56aba157896
moodledev <moodledev@gmail.com>
2020-01-23 17:09:23 -0600

    Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_typorepo';
-$plugin->version = 2020011701;
-$plugin->release = '1.0';
+$plugin->version = 2020012300;
+$plugin->release = '1.1';
 $plugin->requires = 2017051509;
 $plugin->maturity = MATURITY_BETA;
